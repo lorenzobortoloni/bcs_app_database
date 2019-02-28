57971ce192b6f75f1f22cd6d9bbc482289f004dc
lorenzobortoloni <lorenzo.bortoloni@gmail.com>
2019-02-28 16:28:41 +0100

    Minor improvements

--- rootfs/tabelle.sql
+++ rootfs/tabelle.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tabella_utenti (
-    username       varchar(30),
+    username       varchar(30) primary key,
     password       varchar(30),
     userid         int
 );
@@ -7,11 +7,12 @@ CREATE TABLE IF NOT EXISTS tabella_utenti (
 CREATE TABLE IF NOT EXISTS dati_connessione (
     anvizIpAddress    varchar(15),
     anvizPort         int,
-    anvizDeviceCode   int
+    anvizDeviceCode   int,
+    id_configurazione int primary key
 );
 
 --INSERT INTO dati_connessione
---    (192.168.16.3, 5010, 0);
+--    (192.168.16.3, 5010, 0, 1);
 
 COPY tabella_utenti FROM '/tabella_utenti.txt' (DELIMITER '=');
 COPY dati_connessione FROM '/dati_connessione.txt' (DELIMITER '=');
